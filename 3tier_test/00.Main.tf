1308960eb05f80f4a3b5ee4f879e7c546b5ec596
yyuncha <82304668+yyuncha@users.noreply.github.com>
2024-03-04 02:02:25 +0900

    Update 00.Main.tf

--- 3tier_test/00.Main.tf
+++ 3tier_test/00.Main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key = "AKIASPA4ZI6DHI7VX636"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
   region = "ap-northeast-2"
 }
